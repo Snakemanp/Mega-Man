52564c76f849a110ee2c2ecee38c6303940cc3ff
Preetham Battula <bpreetham05@gmail.com>
2024-07-11 16:51:50 +0530

    Removed Code

--- mario/vec4.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "vec2.h"
-
-class vec4 {
-public:
-	vec2 xlimit, ylimit;
-	vec4(vec2& x, vec2& y) : xlimit(x), ylimit(y) {}
-	vec4(float xl, float xr, float yt, float yb) : xlimit(xl, xr), ylimit(yt, yb) {}
-};
